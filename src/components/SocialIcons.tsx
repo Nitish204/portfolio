1c1812500d55622ad4f12fb87679362e5fd35b39
Vishwakarma Nitish <nanivish234@gmail.com>
2026-08-09 19:30:39 +0530

    Remove Twitter icon and update resume link
    Removed Twitter icon link and updated resume link.

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -2,7 +2,6 @@ import {
   FaGithub,
   FaInstagram,
   FaLinkedinIn,
-  FaXTwitter,
 } from "react-icons/fa6";
 import "./styles/SocialIcons.css";
 import { TbNotes } from "react-icons/tb";
@@ -70,17 +69,17 @@ const SocialIcons = () => {
           </a>
         </span>
         <span>
-          <a href="https://x.com" target="_blank">
-            <FaXTwitter />
-          </a>
-        </span>
-        <span>
           <a href="https://www.instagram.com" target="_blank">
             <FaInstagram />
           </a>
         </span>
       </div>
-      <a className="resume-button" href="#">
+      <a
+        className="resume-button"
+        href="/resume.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <HoverLinks text="RESUME" />
         <span>
           <TbNotes />
